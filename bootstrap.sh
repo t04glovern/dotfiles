7531d78ceef1424f9bd2d762671c1e020bd86b6e
Nathan Glover <nathan@glovers.id.au>
2019-03-16 13:23:11 +0800

    sync

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,9 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+        --exclude "brew.sh" \
+        --exclude "brew-cask.sh" \
+        --exclude "init/vscode/" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
