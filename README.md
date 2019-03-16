4fc07eac7316ac80f4120ffa9ff8a2cca6f15861
Nathan Glover <nathan@glovers.id.au>
2019-03-16 12:50:39 +0800

    sync

--- README.md
+++ README.md
@@ -81,6 +81,12 @@ When setting up a new Mac, you may want to install some common [Homebrew](https:
 ./brew.sh
 ```
 
+### Install Homebrew Casks
+
+```bash
+./brew-cask.sh
+```
+
 Some of the functionality of these dotfiles depends on formulae installed by `brew.sh`. If you don’t plan to run `brew.sh`, you should look carefully through the script and manually install any particularly important ones. A good example is Bash/Git completion: the dotfiles use a special version from Homebrew.
 
 ## Feedback
--- brew.sh
+++ brew.sh
@@ -8,9 +8,13 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
@@ -19,15 +23,13 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
 brew install bash
 brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
 # Install `wget` with IRI support.
@@ -41,7 +43,8 @@ brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
 brew install screen
-brew install homebrew/php/php56 --with-gmp
+brew install php
+brew install gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
@@ -49,78 +52,13 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Cask/brew requirements
-brew cask install java
-brew install maven
-brew install jenv
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
 brew install ack
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
 brew install rename
-brew install rlwrap
 brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
-
-# General Casks
-brew cask install transmission
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install dropbox
-brew cask install cisco-spark
-brew cask install keybase
-brew cask install istat-menus
-brew cask install intel-power-gadget
-brew cask install timing
-brew cask install gitkraken
-brew cask install private-internet-access
-brew cask install parallels
-brew cask install vlc
-
-# Developer Tools - General
-brew cask install visual-studio-code
-brew cask install sourcetree
-brew cask install ganache
-
-# Developer Tools - JetBrains Professional
-brew cask install intellij-idea
-brew cask install pycharm
-brew cask install webstorm
 
 # Remove outdated versions from the cellar.
 brew cleanup
